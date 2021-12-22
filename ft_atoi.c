b1959758a6705d6f0e139e674b91fda459ae82f5
jpikkuma <jpikkuma@student.hive.fi>
2021-12-23 01:31:36 +0200

    removed unnecessary variable

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jpikkuma <jpikkuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 00:31:35 by jpikkuma          #+#    #+#             */
-/*   Updated: 2021/12/14 23:27:19 by jpikkuma         ###   ########.fr       */
+/*   Updated: 2021/12/23 01:28:53 by jpikkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,17 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
 	int	value;
 	int	sign;
 
-	i = 0;
 	value = 0;
 	sign = 1;
-	while (ft_isspace(str[i]))
-		++i;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
 			sign *= -1;
-		++i;
-	}
-	while (ft_isdigit(str[i]))
-	{
-		value = value * 10 + str[i] - '0';
-		++i;
-	}
+	while (ft_isdigit(*str))
+		value = value * 10 + *str++ - '0';
 	return (sign * value);
 }
