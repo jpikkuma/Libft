332fdafd6cb26b24a51dab44c06d98f80c4b0250
jpikkuma <jpikkuma@student.hive.fi>
2021-12-23 02:09:26 +0200

    removed unused variable

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jpikkuma <jpikkuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 18:37:03 by jpikkuma          #+#    #+#             */
-/*   Updated: 2021/12/23 02:05:43 by jpikkuma         ###   ########.fr       */
+/*   Updated: 2021/12/23 02:08:38 by jpikkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,17 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*fresh;
-	char	*ret;
 	size_t	s1len;
 	size_t	s2len;
 
-	if (!s1 || !s2)
-		return (NULL);
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
+	if (!s1 || !s2)
+		return (NULL);
 	fresh = ft_strnew(s1len + s2len);
-	ret = fresh;
 	if (!fresh)
 		return (NULL);
 	ft_memcpy((void *)fresh, (void *)s1, s1len);
 	ft_memcpy((void *)(fresh + s1len), (void *)s2, s2len + 1);
-	return (ret);
+	return (fresh);
 }
